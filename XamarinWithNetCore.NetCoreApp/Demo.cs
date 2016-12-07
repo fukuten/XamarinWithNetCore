00de7909b4d537aff19f82dd9d2fdea51a4cfd89
tsubakimoto <tkmbmbr@gmail.com>
2016-12-07 21:33:27 +0900

    [add] .NET Core Project

---
+++ XamarinWithNetCore.NetCoreApp/Demo.cs
@@ -0,0 +1,10 @@
+﻿namespace XamarinWithNetCore.NetCoreApp
+{
+    public static class Demo
+    {
+        public static string Greet()
+        {
+            return "Hello .NET Core App !!";
+        }
+    }
+}
