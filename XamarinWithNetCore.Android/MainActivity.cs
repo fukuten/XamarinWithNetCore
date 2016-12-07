2b97355ad25e869f708bc8470776a5c66f6c9751
tsubakimoto <tkmbmbr@gmail.com>
2016-12-07 20:48:25 +0900

    [add] Xamarin BlankApp

---
+++ XamarinWithNetCore.Android/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace XamarinWithNetCore.Android
+{
+    [Activity(Label = "XamarinWithNetCore.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var textView = FindViewById<TextView>(Resource.Id.textView1);
+
+            var button1 = FindViewById<Button>(Resource.Id.button1);
+            button1.Click += (sender, e) =>
+            {
+                //textView.Text = 
+            };
+
+            var button2 = FindViewById<Button>(Resource.Id.button2);
+            button2.Click += (sender, e) =>
+            {
+                //textView.Text = 
+            };
+
+            var button3 = FindViewById<Button>(Resource.Id.button3);
+            button3.Click += (sender, e) =>
+            {
+                //textView.Text = 
+            };
+        }
+    }
+}
+
